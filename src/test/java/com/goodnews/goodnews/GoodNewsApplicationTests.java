1cb4ea4b2ecf5d0f30ab71a730a8d601af986616
임창현 <ckdgus777@naver.com>
2024-01-09 14:45:22 +0900

    Start Spring Boot
    Start Spring Boot

---
+++ src/test/java/com/goodnews/goodnews/GoodNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodnews.goodnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
